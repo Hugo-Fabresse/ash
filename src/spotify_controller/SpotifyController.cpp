8c6f993e4a4c111fae3248d5f374f86c0a9434ff
Hugo-Fabresse <hugo.fabresse@epitech.eu>
2026-05-16 14:45:37 +0200

    feat(spotify_controller): add Warning with qWarning if Spotify is not running
    - include <QtLogging>
    - check if dbus->isValid() return false
    - use qWarning with message "Error: Spotify is not running."

--- src/spotify_controller/SpotifyController.cpp
+++ src/spotify_controller/SpotifyController.cpp
@@ -1,9 +1,13 @@
 // ash - src/spotify_controller/SpotifyController.cpp
 
 #include "SpotifyController.h"
+#include <QtLogging>
 
 SpotifyController::SpotifyController() {
 	dbus = std::make_unique<QDBusInterface>("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2", "org.mpris.MediaPlayer2.Player");
+	if (!dbus->isValid()) {
+		qWarning() << "Error: Spotify is not running.";
+	}
 }
 
 void SpotifyController::playPause() {
